8eba6c0bebe7973b91364244c1d607727b3c6c3a
Erick <delano.erick@gmail.com>
2012-03-27 16:11:14 +0700

    first commit

--- device_gio.mk
+++ device_gio.mk
@@ -24,20 +24,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 DEVICE_PACKAGE_OVERLAYS := device/samsung/gio/overlay
 
-# Discard inherited values and use our own instead.
-PRODUCT_NAME := gio
-PRODUCT_DEVICE := gio
-PRODUCT_MODEL := GT-S5660
-
 PRODUCT_PACKAGES += \
     librs_jni \
-    Gallery3d \
     libOmxCore \
-    libOmxVidEnc \
     make_ext4fs \
     brcm_patchram_plus \
     sec_touchscreen.kcm \
@@ -47,6 +40,15 @@ PRODUCT_PACKAGES += \
     gps.gio \
     setup_fs \
     FM \
+    SamsungServiceMode \
+    rzscontrol \
+    bdaddr_read \
+    dump_image \
+    e2fsck \
+    setup_fs \
+    erase_image \
+    flash_image \
+    screencap \
     dexpreopt
 
 # proprietary side of the device
@@ -58,14 +60,13 @@ PRODUCT_COPY_FILES += \
     device/samsung/gio/prebuilt/abtfilt:system/bin/abtfilt \
     device/samsung/gio/prebuilt/memsicd:system/bin/memsicd \
     device/samsung/gio/prebuilt/qmuxd:system/bin/qmuxd \
-    device/samsung/gio/prebuilt/wlan_mac:system/bin/wlan_mac \
-    device/samsung/gio/prebuilt/wlan_tool:system/bin/wlan_tool \
     device/samsung/gio/prebuilt/wmiconfig:system/bin/wmiconfig \
+    device/samsung/gio/prebuilt/02gio:system/etc/init.d/02gio \
     device/samsung/gio/prebuilt/qwerty.kl:system/usr/keylayout/qwerty.kl \
     device/samsung/gio/prebuilt/sec_touchscreen.kl:system/usr/keylayout/sec_touchscreen.kl \
-    device/samsung/gio/prebuilt/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
-    device/samsung/gio/prebuilt/sec_touchscreen.kl:system/usr/keylayout/sec_jack.kl \
-    device/samsung/gio/prebuilt/sec_touchscreen.kl:system/usr/keylayout/sec_key.kl   
+    device/samsung/gio/prebuilt/sec_jack.kl:system/usr/keylayout/sec_jack.kl \
+    device/samsung/gio/prebuilt/sec_key.kl:system/usr/keylayout/sec_key.kl
+  
 
 # fstab
 PRODUCT_COPY_FILES += \
@@ -73,9 +74,9 @@ PRODUCT_COPY_FILES += \
 
 # Init
 PRODUCT_COPY_FILES += \
-    device/samsung/gio/prebuilt/init.gt-s5660v.rc:root/init.gt-s5660v.rc \
+    device/samsung/gio/prebuilt/init.gt-s5660.rc:root/init.gt-s5660.rc \
     device/samsung/gio/prebuilt/GIO.rle:root/GIO.rle \
-    device/samsung/gio/prebuilt/ueventd.gt-s5660v.rc:root/ueventd.gt-s5660v.rc
+    device/samsung/gio/prebuilt/ueventd.gt-s5660.rc:root/ueventd.gt-s5660.rc
 
 # Audio
 PRODUCT_COPY_FILES += \
@@ -87,9 +88,11 @@ PRODUCT_COPY_FILES += \
     device/samsung/gio/prebuilt/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     device/samsung/gio/prebuilt/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
     device/samsung/gio/prebuilt/hostapd:system/bin/hostapd \
+    device/samsung/gio/prebuilt/wlan_mac:system/bin/wlan_mac \
+    device/samsung/gio/prebuilt/wlan_tool:system/bin/wlan_tool \
     device/samsung/gio/prebuilt/hostapd.conf:system/etc/wifi/hostapd.conf
     
-# Install the features available on this device.
+# Hardware
 PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
@@ -97,6 +100,8 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml
 
 #Kernel Modules
@@ -107,7 +112,6 @@ PRODUCT_COPY_FILES += \
     device/samsung/gio/prebuilt/ar6000.ko:system/wifi/ar6000.ko \
     device/samsung/gio/prebuilt/cifs.ko:system/lib/modules/cifs.ko \
     device/samsung/gio/prebuilt/tun.ko:system/lib/modules/tun.ko \
-    device/samsung/gio/prebuilt/librasdioif.ko:system/lib/modules/librasdioif.ko \
     device/samsung/gio/prebuilt/zram.ko:system/lib/modules/zram.ko
     
 #Kernel Modules for Recovery (RFS)
@@ -159,10 +163,12 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 
 # This should not be needed but on-screen keyboard uses the wrong density without it.
 PRODUCT_PROPERTY_OVERRIDES += \
-    qemu.sf.lcd_density=160
+    qemu.sf.lcd_density=160 \
+    hw.keyboards.0.devname=qwerty
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1 \
     ro.setupwizard.enable_bypass=1 \
     ro.media.dec.jpeg.memcap=20000000 \
     dalvik.vm.lockprof.threshold=500 \
@@ -172,8 +178,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dexopt-data-only=1 \
     ro.opengles.version=131072  \
     ro.compcache.default=0
+    
 # See comment at the top of this file. This is where the other
 # half of the device-specific product definition file takes care
 # of the aspects that require proprietary drivers that aren't
 # commonly available
 $(call inherit-product-if-exists, vendor/samsung/gio/gio-vendor-blobs.mk)
+
+PRODUCT_NAME := gio
+PRODUCT_DEVICE := gio
+PRODUCT_MODEL := GT-S5660
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
